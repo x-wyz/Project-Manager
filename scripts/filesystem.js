b1e86276eed50c4e1c920ba2c73aec0ab1ad12ba
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-08-26 01:19:17 -0700

    dowgraded electron version 10 -> 9

---
+++ scripts/filesystem.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { dialog } = require('electron').remote;
+console.log(dialog);
+
+// const test = document.getElementById('tool-save-project');
+// test.addEventListener('click', () => {
+// 	let content = "Hello this is the content";
+// 	dialog.showSaveDialog((filename) => {
+// 		if(filename === undefined) {
+// 			console.log("error");
+// 			return;
+// 		}
+
+// 		fs.writeFile(filename, content, (err) => {
+// 			if (err) return;
+// 		})
+// 	})
+// })
