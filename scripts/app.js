44c5baea80358ab7559f21e2d994e093baec3003
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-08-25 23:19:11 -0700

    functionality for major tasks

--- scripts/app.js
+++ scripts/app.js
@@ -49,8 +49,87 @@ const APP = {
 	}
 }
 
+const majorTasks = {};
 const minorTasks = {};
 const completedTasks = {};
+
+function addMajorTask() {
+	if (!APP.newTaskText.value) return;
+
+	 function addNotesHandler(field, container) {
+	 	const newNote = document.createElement('div');
+
+	 	const noteText = createElement(field.value, ['notes-text']);
+	 	const removeNote = createElement('RM', ['remove-note'], 'button');
+
+	 	compileElement(newNote, [noteText, removeNote]);
+
+	 	const uuid2 = generateId();
+
+	 	container.appendChild(newNote);
+
+	 	majorTasks[uuid].notes[uuid2] = {
+	 		element: newNote,
+	 		id: uuid2,
+	 		text: noteText.innerHTML
+	 	}
+
+	 	removeNote.addEventListener('click', () => removeHandler(uuid2, majorTasks[uuid].notes));
+
+	 	field.value = "";
+	 }
+
+	const task = document.createElement('div');
+	const general = document.createElement('div');
+	const notes = document.createElement('div');
+	const newNotes = document.createElement('div');
+
+	const taskText = createElement(APP.newTaskText.value, ['task-text']);
+	const addNotes = createElement('+Note', ['task-note', 'task-btn'], 'button');
+	const taskLock = createElement('LK', ['task-complete', 'task-btn'], 'button');
+	const taskRemove = createElement('RM', ['task-complete', 'task-btn'], 'button');
+
+	const inputField = document.createElement("input");
+	const addNotesBtn = createElement("+", ['add-notes', 'notes-btn'], 'button');
+
+	inputField.setAttribute('type', 'text');
+	inputField.classList.add("new-notes-field");
+
+	compileElement(newNotes, [inputField, addNotesBtn]);
+
+	newNotes.classList.add('hidden');
+
+	compileElement(general, [taskText, addNotes, taskLock, taskRemove]);
+	compileElement(task, [general, newNotes, notes]);
+
+	const uuid = generateId();
+
+	task.id = uuid;
+
+	task.classList.add('generated-task');
+
+	APP.majorContainer.appendChild(task);
+
+	majorTasks[uuid] = {
+		element: task,
+		id: uuid,
+		text: taskText.innerHTML,
+		notes: {},
+		locked: false,
+		buttons: [addNotes, taskRemove],
+		newNotesField: newNotes,
+		hidden: true
+	}
+
+	APP.newTaskText.value = "";
+
+	taskRemove.addEventListener('click', () => removeHandler(uuid, majorTasks));
+	taskLock.addEventListener('click', () => lockHandler(uuid));
+	addNotes.addEventListener('click', () => notesHandler(uuid));
+
+	addNotesBtn.addEventListener('click', () => addNotesHandler(inputField, notes));
+
+}
 
 function addMinorTask() {
 	if (!APP.newTaskText.value) return;
@@ -58,17 +137,20 @@ function addMinorTask() {
 	const task = document.createElement('div');
 
 	const taskText = createElement(APP.newTaskText.value, ['task-text']);
-	const taskCurrent = createElement('CU', ['task-current', 'task-btn']);
-	const taskComplete = createElement('CP', ['task-complete', 'task-btn']);
-	const taskRemove = createElement('RM', ['task-complete', 'task-btn']);
+	const taskCurrent = createElement('CU', ['task-current', 'task-btn'], 'button');
+	const taskComplete = createElement('CP', ['task-complete', 'task-btn'], 'button');
+	const taskRemove = createElement('RM', ['task-complete', 'task-btn'], 'button');
 
 	compileElement(task, [taskText, taskCurrent, taskComplete, taskRemove]);
+
+	task.classList.add('generated-task');
 
 	const uuid = generateId();
 
 	task.id = uuid;
 
 	APP.minorContainer.appendChild(task);
+
 	minorTasks[uuid] = {
 		element: task,
 		id: uuid,
@@ -80,10 +162,37 @@ function addMinorTask() {
 	}
 
 	taskCurrent.addEventListener('click', () => currentHandler(uuid));
-	taskRemove.addEventListener('click', () => removeHandler(uuid));
-	taskComplete.addEventListener('click', () => completeHandler(uuid));
+	taskRemove.addEventListener('click', () => removeHandler(uuid, minorTasks));
+	taskComplete.addEventListener('click', () => completeHandler(uuid, minorTasks));
 
 	APP.newTaskText.value = "";
+}
+
+function notesHandler(id) {
+	majorTasks[id].hidden = !majorTasks[id].hidden;
+	if (majorTasks[id].hidden) {
+		majorTasks[id].newNotesField.classList.add('hidden');
+	}
+	else {
+		majorTasks[id].newNotesField.classList.remove('hidden');
+	}
+}
+
+function lockHandler(id) {
+	majorTasks[id].locked = !majorTasks[id].locked;
+	for (let i = 0; i < majorTasks[id].buttons.length; i++) {
+		if(majorTasks[id].locked) {
+			majorTasks[id].buttons[i].setAttribute("disabled", "true");
+		} else {
+			majorTasks[id].buttons[i].removeAttribute("disabled");
+		}
+	}
+
+	if (majorTasks[id].locked) {
+		majorTasks[id].element.classList.add('isLocked');
+	} else {
+		majorTasks[id].element.classList.remove('isLocked');
+	}
 }
 
 function currentHandler(id) {
@@ -92,12 +201,12 @@ function currentHandler(id) {
 	APP.currentTimer = timer(minorTasks[id].timer);
 }
 
-function removeHandler(id) {
-	minorTasks[id].element.remove();
-	delete minorTasks[id];
+function removeHandler(id, container) {
+	container[id].element.remove();
+	delete container[id];
 }
 
-function completeHandler(id) {
+function completeHandler(id, container) {
 	const newCompleted = document.createElement('div');
 
 	const completedTaskText = minorTasks[id].text;
@@ -120,7 +229,9 @@ function completeHandler(id) {
 	}
 
 	APP.completedContainer.appendChild(newCompleted);
-	removeHandler(id);
+	removeHandler(id, container);
+
+	removeCompleted.addEventListener('click', () => removeHandler(id, completedTasks));
 }
 
 function timer(obj) {
@@ -140,3 +251,4 @@ function formatTime(timerObj) {
 }
 
 APP.addMinorBtn.addEventListener('click', () => addMinorTask());
+APP.addMajorBtn.addEventListener('click', () => addMajorTask());
