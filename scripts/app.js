f2dc8446f8355dab049f4d7bd5249f4d66011465
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-08-25 20:00:50 -0700

    completed functionality for minor tasks

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
 const getElement = (id) => document.getElementById(id);
 
+function createElement(txt, classes, type='p'){
+	const element = document.createElement(type);
+	element.innerHTML = txt;
+	for (let i = 0; i < classes.length; i++) {
+		element.classList.add(classes[i]);
+	}
+	return element;
+}
+
+function compileElement(ele, elements){
+	for (let i = 0; i < elements.length; i++) {
+		ele.appendChild(elements[i]);
+	}
+}
+
 const { v4: generateId } = require('uuid');
 
 const APP = {
@@ -28,11 +43,100 @@ const APP = {
 	currentTaskTimer: getElement('current-timer'),
 	// Additional Tools / FS
 	// ****
+	currentTimer: null,
+	clearTimer: function(){
+		clearInterval(this.currentTimer);
+	}
 }
 
-const majorTasks = {}
-const minorTasks = {}
-const scheduleTasks = {}
-const completedTasks = {}
+const minorTasks = {};
+const completedTasks = {};
 
+function addMinorTask() {
+	if (!APP.newTaskText.value) return;
 
+	const task = document.createElement('div');
+
+	const taskText = createElement(APP.newTaskText.value, ['task-text']);
+	const taskCurrent = createElement('CU', ['task-current', 'task-btn']);
+	const taskComplete = createElement('CP', ['task-complete', 'task-btn']);
+	const taskRemove = createElement('RM', ['task-complete', 'task-btn']);
+
+	compileElement(task, [taskText, taskCurrent, taskComplete, taskRemove]);
+
+	const uuid = generateId();
+
+	task.id = uuid;
+
+	APP.minorContainer.appendChild(task);
+	minorTasks[uuid] = {
+		element: task,
+		id: uuid,
+		text: taskText.innerHTML,
+		timer: {
+			minutes: 0,
+			seconds: 0
+		}
+	}
+
+	taskCurrent.addEventListener('click', () => currentHandler(uuid));
+	taskRemove.addEventListener('click', () => removeHandler(uuid));
+	taskComplete.addEventListener('click', () => completeHandler(uuid));
+
+	APP.newTaskText.value = "";
+}
+
+function currentHandler(id) {
+	APP.clearTimer();
+	APP.currentTaskText.innerHTML = minorTasks[id].text;
+	APP.currentTimer = timer(minorTasks[id].timer);
+}
+
+function removeHandler(id) {
+	minorTasks[id].element.remove();
+	delete minorTasks[id];
+}
+
+function completeHandler(id) {
+	const newCompleted = document.createElement('div');
+
+	const completedTaskText = minorTasks[id].text;
+	const completedTaskTime = formatTime(minorTasks[id].timer);
+
+	const completedText = createElement(completedTaskText, ['completed-text']);
+	const completedTime = createElement(completedTaskTime, ['completed-time']);
+	const removeCompleted = createElement('RM', ['remove']);
+
+	compileElement(newCompleted, [completedText, completedTime, removeCompleted]);
+
+	completedTasks[id] = {
+		element: newCompleted,
+		id: id,
+		text: completedTaskText,
+		timer: {
+			minutes: minorTasks[id].timer.minutes,
+			seconds: minorTasks[id].timer.seconds
+		}
+	}
+
+	APP.completedContainer.appendChild(newCompleted);
+	removeHandler(id);
+}
+
+function timer(obj) {
+	function start(){
+		obj.seconds++;
+		if (obj.seconds >= 60) {
+			obj.seconds = 0;
+			obj.minutes++;
+		}
+		APP.currentTaskTimer.innerHTML = formatTime(obj);
+	}
+	return setInterval(start, 1000);
+}
+
+function formatTime(timerObj) {
+	return `${timerObj.minutes > 9 ? timerObj.minutes : '0'+timerObj.minutes}:${timerObj.seconds > 9 ? timerObj.seconds : '0'+timerObj.seconds}`
+}
+
+APP.addMinorBtn.addEventListener('click', () => addMinorTask());
